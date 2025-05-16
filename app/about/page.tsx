883b3810ba500ad82ffdbcb4671e96d00c25c028
yushu <zhuysh18@163.com>
2025-05-16 19:00:36 +1000

    about - Image component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import image1 from "@/public/about-1.jpg";
 
 export const metadata = {
   title: "About",
@@ -36,13 +38,18 @@ export default function Page() {
 
       <div className="col-span-2">
         <Image
-          src="/about-1.jpg"
+          src={image1}
           alt="Family sitting around a fire pit in front of cabin"
         />
       </div>
 
-      <div className="col-span-2">
-        <Image src="/about-2.jpg" alt="Family that manages The Wild Oasis" />
+      <div className="relative aspect-square col-span-2">
+        <Image
+          fill
+          className="object-cover"
+          src="/about-2.jpg"
+          alt="Family that manages The Wild Oasis"
+        />
       </div>
 
       <div className="col-span-3">
@@ -67,12 +74,12 @@ export default function Page() {
           </p>
 
           <div>
-            <a
+            <Link
               href="/cabins"
               className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
             >
               Explore our luxury cabins
-            </a>
+            </Link>
           </div>
         </div>
       </div>
