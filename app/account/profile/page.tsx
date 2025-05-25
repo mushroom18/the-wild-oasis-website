97145d698c431e98749c53b0c33c36ac33cb6725
yushu <zhuysh18@163.com>
2025-05-25 11:18:08 +1000

    add a nested layout

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,8 +1,10 @@
 import SelectCountry from "@/app/_component/SelectCountry";
 
+export const metadata = {
+  title: "Profile",
+};
+
 export default function Page() {
-  // CHANGE
-  const countryFlag = "pt.jpg";
   const nationality = "portugal";
 
   return (
@@ -36,11 +38,6 @@ export default function Page() {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryFlag}
-              alt="Country flag"
-              className="h-5 rounded-sm"
-            />
           </div>
 
           <SelectCountry
--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -1,5 +1,9 @@
 import ReservationCard from "@/app/_component/ReservationCard";
 
+export const metadata = {
+  title: "Reservations",
+};
+
 export default function Page() {
   // CHANGE
   const bookings = [];
