53e102a47e5a7c875cbf2875cb1eddcf10958a18
yushu <zhuysh18@163.com>
2025-08-05 18:15:15 +1000

    create new guest on first sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,4 +1,5 @@
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const authOptions = {
   providers: [
@@ -10,6 +11,21 @@ export const authOptions = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch (error) {
+        console.error("Error fetching guest:", error);
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
